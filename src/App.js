757fc6c762aa899b83385e43ba4e348086ab287b
khanh.le1 <khanh.le1@onemount.com>
2024-01-22 16:00:01 +0700

    'UI'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
 
+import Navbar from './components/navbar';
+import Hero from './components/hero';
+import About  from './components/about';
+import Services from './components/services';
+import Tours from './components/tours';
+import Footer from './components/footer';
+import './index.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <Navbar/>
+    <Hero/>
+    <About/>
+    <Services/>
+    <Tours/>
+    <Footer/>
+  
+
+    </>
+    )
+  ;
 }
 
 export default App;
---
+++ src/components/navbar.js
@@ -0,0 +1,45 @@
+import logo from '../images/logo.svg'
+import { pageLinks } from '../data';
+import { socialLinks } from '../data';
+const Navbar = ()=> {
+    return (
+        <nav className="navbar">
+        <div className="nav-center">
+          <div className="nav-header">
+            <img src={logo} className="nav-logo" alt="backroads" />
+            <button type="button" className="nav-toggle" id="nav-toggle">
+              <i className="fas fa-bars"></i>
+            </button>
+          </div>
+          {/* <!-- left this comment on purpose --> */}
+          <ul className="nav-links" id="nav-links">
+            {
+                pageLinks.map((pageLink)=>{
+                    return( 
+                    <li key={pageLink.id}>
+                        <a href={pageLink.href} className="nav-link"> {pageLink.text} </a>
+                      </li>)
+                })
+            }
+
+          </ul>
+    
+          <ul className="nav-icons">
+            {
+                socialLinks.map((socialLink)=>{
+                    return (
+                        <li>
+                        <a href={socialLink.href} target="_blank" rel= 'noreferrer' className="nav-icon"
+                          ><i className={socialLink.icon}></i
+                        ></a>
+                      </li>
+                    )
+
+                })
+            }
+          </ul>
+        </div>
+      </nav>
+    )
+}
+export default Navbar
